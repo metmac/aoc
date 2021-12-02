0eed26522af16cc5c5da76b8d80a054df0b1f714
metmac <metmac@mac.com>
2021-12-01 21:50:51 -0800

    cleaned up day2 solution, thx kcon

--- 2021/day02/puzzle.py
+++ 2021/day02/puzzle.py
@@ -1,14 +1,12 @@
-def part1():
-    a = [ {"op": x[0],"val": x[1] } for x in [x.strip().split(" ") for x in open("./input.txt", "r")] ]
-    fwd = sum([int(x["val"]) for x in a if x["op"] == "forward"])
-    up = sum([int(x["val"]) for x in a if x["op"] == "up"])
-    down = sum([int(x["val"]) for x in a if x["op"] == "down"])
+def part1(data):
+    fwd = sum([int(x["val"]) for x in data if x["op"] == "forward"])
+    up = sum([int(x["val"]) for x in data if x["op"] == "up"])
+    down = sum([int(x["val"]) for x in data if x["op"] == "down"])
     print((down - up) * fwd)
 
-def part2():
-    a = [ {"op": x[0],"val": x[1] } for x in [x.strip().split(" ") for x in open("./input.txt", "r")] ]
+def part2(data):
     hoz = dep =  aim = 0
-    for move in a:
+    for move in data:
         if move["op"] == "forward":
             hoz +=  int(move["val"])
             dep += ( int(move["val"]) * aim)
@@ -17,3 +15,7 @@ def part2():
         elif move["op"] == "down":
             aim +=  int(move["val"])
     print(hoz * dep)
+
+a = [ {"op": x[0],"val": x[1] } for x in [x.strip().split(" ") for x in open("./input.txt", "r")] ]
+part1(a)
+part2(a)
